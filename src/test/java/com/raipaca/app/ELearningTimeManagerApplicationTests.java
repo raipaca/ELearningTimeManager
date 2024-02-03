d5efe0280cd61abc8b7bc45c707b0b70bd8e690a
aipaca <aipaca@sample.notExist.jp>
2024-02-04 08:16:35 +0900

    初期状態
    プロジェクト作成直後の状態

---
+++ src/test/java/com/raipaca/app/ELearningTimeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raipaca.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningTimeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
