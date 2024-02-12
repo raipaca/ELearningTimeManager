6d62f0f88b08fed72e5c487bd7876f531d480e3a
aipaca <aipaca@sample.notExist.jp>
2024-02-12 13:55:58 +0900

    利用者登録画面に遷移しない #002
    ログアウト画面にリダイレクトした時に画面が表示されてしまう不具合修正時に、利用者登録画面にも影響するように修正してしまった。
    不具合番号：#002

--- src/main/java/com/raipaca/app/config/ApplicationConfig.java
+++ src/main/java/com/raipaca/app/config/ApplicationConfig.java
@@ -32,7 +32,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
 		var bean = new FilterRegistrationBean<AuthFilter>(new AuthFilter());
 		bean.addUrlPatterns("/logout");
 		bean.addUrlPatterns("/learning/*");
-		bean.addUrlPatterns("/user/*");
+		bean.addUrlPatterns("/user/edit*");
 		return bean;
 	}
 
